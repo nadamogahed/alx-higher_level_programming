4e4f9f06125e05ad8e51992e02460ea7a8054917
nadamogahed <nadamarcy25@gmail.com>
2023-10-31 19:27:28 -0700

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(97, 123):
+        if ord(c) == i:
+            c = chr(ord(c) - 32)
+            print("{}".format(c))
+    print("{}".format(c))
