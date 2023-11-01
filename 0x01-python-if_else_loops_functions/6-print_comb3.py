9c5a1ee0669d32a268faa8d16eed1de9328c63e1
nadamogahed <nadamarcy25@gmail.com>
2023-10-31 17:56:12 -0700

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for x in range(1, 10):
-        if i == x:
-            continue
-        elif i == 8 and x == 9:
-            print("{}{}".format(i,x))
-        else:
-            print("{}{}, ".format(i,x), end='')
+for i in range(1, 9):
+    for x in range(0, 10):
+        if i != x:
+            if i == 8 and x == 9:
+                print("{}{}".format(i,x))
+            else:
+                print("{}{}, ".format(i,x), end='')
