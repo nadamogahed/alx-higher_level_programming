d8add989088b382d3c8466d6d5fc814a4d7ea624
nadamogahed <nadamarcy25@gmail.com>
2023-11-03 00:23:35 -0700

    task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
+
 def main():
     a = 10
     b = 5
@@ -7,6 +9,7 @@ def main():
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
 
 if __name__ == "__main__":
     main()
