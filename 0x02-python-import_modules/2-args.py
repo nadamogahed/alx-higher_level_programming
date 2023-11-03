820862c0b55592db3ec99ee9ce25344bbe4d3d17
nadamogahed <nadamarcy25@gmail.com>
2023-11-03 01:02:01 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def main(argv):
+    print("{} arguments".format(len(argv)), end='')
+    if len(argv) < 0:
+        print(".")
+    else:
+        print(":")
+    for i in range(len(argv)):
+        print("{} {}".format(i + 1, argv[i]))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
