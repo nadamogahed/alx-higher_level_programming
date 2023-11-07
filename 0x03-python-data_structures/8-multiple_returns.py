03adb32224f81a03d41320511c303da2186daf69
nadamogahed <nadamarcy25@gmail.com>
2023-11-07 12:43:54 -0800

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence is "":
         sentence[0] = None
     tuple_re = (len(sentence), sentence[0])
     return tuple_re
