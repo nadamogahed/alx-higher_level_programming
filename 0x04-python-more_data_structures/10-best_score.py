c055e478d45c84d5289a83e66a44901613887328
nadamogahed <nadamarcy25@gmail.com>
2023-11-08 12:40:59 -0800

    task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_num = max(a_dictionary.values())
+    for key, value in dictionary.items():
+        if value == certain_value:
+            return key
+        else:
+            return None
+
